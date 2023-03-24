de11303ee6d45821457e5655578d0f1b997af9f0
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-23 22:29:37 -0300

    Add files via upload

---
+++ Condições de Repetição for.js
@@ -0,0 +1,21 @@
+var c = 1
+while(c <= 5) {
+    console.log(c)
+    c = c + 1
+}
+
+//--------------------------------
+//Diferenças das estruturas de repetição
+
+var c = 1
+do(c<= 5) {
+    c= c + 1
+} while (c <= 5) 
+
+
+//--------------------------------
+console.log('Vai começar...')
+for(c = 1; c<=5 ; c++) {
+    console.log(c)
+}
+console.log('FIM')
