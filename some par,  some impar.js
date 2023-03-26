5c1198068e758f0467da5f481aa645d34eca1df4
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-26 16:48:26 -0300

    Add files via upload

---
+++ some par,  some impar.js
@@ -0,0 +1,6 @@
+valor = 3
+if (valor %2 == 0 ){
+    console.log(`Valor A = ${valor} é um número par, e a soma de ${valor} + 5 vai ser = ${valor + 5}`)
+} else if(valor %3 == 0){
+    console.log(`Valor A = ${valor} é um número impar, e a soma de ${valor} + 8 vai ser = ${valor + 8}`)
+}
