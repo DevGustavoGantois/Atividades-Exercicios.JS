331821380301f86b83d6493493858672e7730a26
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-26 13:12:48 -0300

    Add files via upload

---
+++ Número Maior, Número Menor.js
@@ -0,0 +1,7 @@
+var numero1 = 4
+var numero2 = 3
+if (numero1 < numero2) {
+    console.log(`O ${numero1} é menor que o ${numero2}`)
+} else {
+    console.log(`O ${numero1} é maior que o ${numero2}`)
+}
