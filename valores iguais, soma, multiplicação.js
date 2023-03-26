7e43a8cc20d31529b37b0e05016c32fcf9b0c07b
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-26 16:05:22 -0300

    Add files via upload

---
+++ valores iguais, soma, multiplicação.js
@@ -0,0 +1,7 @@
+valorA = 2
+valorB = 3
+if (valorA = valorB) {
+    console.log(`O valor A = ${valorA}, o valor B = ${valorB} e eles são iguais, a soma = ${valor + valorB} `)
+} else if(valorA != valorB) {
+    console.log(`O valor A = ${valorA}, o valor B = ${valorB} e eles são diferentes, a multiplicação = ${valorA * valorB} `)
+}
