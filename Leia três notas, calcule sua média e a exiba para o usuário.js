f9f3b1caf6c3794dc37e95b10850784011a052c3
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-29 21:07:26 -0300

    Add files via upload

---
+++ Leia três notas, calcule sua média e a exiba para o usuário.js
@@ -0,0 +1,6 @@
+var nomeUsuario = 'Augusto'
+var nota1 = 7.5
+var nota2 = 6.0
+var nota3 = 5.5
+var media = (nota1 + nota2 + nota3) / 3
+console.log(`O nome do usuário é ${nomeUsuario} ele tirou na primeira nota ${nota1} na segunda nota ${nota2} e na terceira nota ${nota3} e a média dele é de ${media.toFixed(1)}`)
