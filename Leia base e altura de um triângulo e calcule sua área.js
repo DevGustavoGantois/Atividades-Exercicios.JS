fb907ea7088f5b52de943eff49f6628babdaecf8
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-29 21:17:44 -0300

    Add files via upload

---
+++ Leia base e altura de um triângulo e calcule sua área.js
@@ -0,0 +1,5 @@
+var BaseTriangulo = 2
+var AlturaTriangulo = 3
+var AreaDoTraingulo = BaseTriangulo * AlturaTriangulo / 2
+
+console.log (`A área do triângulo vai ser constiuida pela base = ${BaseTriangulo} , a altura = ${AlturaTriangulo} e a formula da área será ${BaseTriangulo}x${AlturaTriangulo}/2 = ${AreaDoTraingulo}`)
