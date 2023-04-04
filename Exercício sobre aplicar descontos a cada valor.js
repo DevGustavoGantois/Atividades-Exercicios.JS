1140a1d25007fe9e39e843b74dbecca71edf1ab7
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-04 00:05:33 -0300

    Add files via upload

---
+++ Exercício sobre aplicar descontos a cada valor.js
@@ -0,0 +1,8 @@
+const ValorDaCompra = 500;
+
+for (let i = 1; i < 26; i++) {
+    console.log(`Valor da compra: ${ValorDaCompra} 
+    Porcentagem de desconto: ${i}% Valor final ${ValorDaCompra * (1-(i/100))} `);
+
+}
+
