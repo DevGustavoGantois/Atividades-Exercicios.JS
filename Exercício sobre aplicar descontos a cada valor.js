f754d3f573fe4d021422f38b3c4d2f506441bfc2
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-04 00:05:10 -0300

    Delete Exercício sobre aplicar descontos a cada valor.js

--- Exercício sobre aplicar descontos a cada valor.js
+++
@@ -1,8 +0,0 @@
-const ValorDaCompra = 500;
-
-for (let i = 1; i < 26; i++) {
-    console.log(`Valor da compra: ${ValorDaCompra} 
-    Porcentagem de desconto: ${i}% Valor final ${ValorDaCompra * (1-(i/200))} `);
-
-}
-
