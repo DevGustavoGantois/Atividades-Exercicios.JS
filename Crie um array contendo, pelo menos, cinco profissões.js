3d3b2f97f5b498294fe6598c66d5b615a6ab405d
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-08 01:09:20 -0300

    Add files via upload

---
+++ Crie um array contendo, pelo menos, cinco profissões.js
@@ -0,0 +1,20 @@
+
+let profissões = ["Médico", "Engenheiro", "Professor", "Programador", "Agricultor"];
+
+
+profissões[0] = "Veterinário";
+
+profissões.pop();
+
+profissões.push("Piloto")
+
+profissões.slice(0,1)
+
+profissões.slice(0,2)
+
+profissões[2] = "designer";
+
+profissões[3] = "carpinteiro";
+
+
+console.log(profissões)
