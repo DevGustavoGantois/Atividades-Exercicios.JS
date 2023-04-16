ed0a4dd712f5d7d3970e6974e51b7a0dd51e0a8e
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-16 11:28:38 -0300

    Add files via upload

---
+++ Usando a arrow Fanction em um exercício.js
@@ -0,0 +1,15 @@
+const QuadradoNumero = (numero) => {
+
+    return numero**2
+}
+
+
+console.log("O quadrado de 4 é =")
+console.log(QuadradoNumero(4))
+
+
+//Criei uma constante "QuadradoNumero" que vai ser igual a numero.
+//Criei uma ArrowFunction.
+//Depois disso retornei o número ao quadrado "**2"
+//dei o console.log para mostrar o resultado 
+//Chamei a constante "QuadradoNumero" e substitui o número por 4.
