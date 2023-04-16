199e65e9a22df43e6ac2f189b63e2e4702227cd5
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-15 21:55:23 -0300

    Add files via upload

---
+++ Implemente uma função que receba uma string como parâmetro e devolva a mesma invertida. Ex “Noite”  “etioN”.js
@@ -0,0 +1,15 @@
+function RecebeString(string) {
+    
+    return string.split('').reverse().join('');
+} 
+
+
+console.log(RecebeString('GustavoÉmuitoBonito'))
+
+
+
+//Criei uma function chamada "RecebeString" e dentro da function botei uma (string).
+//retornei a (string) como string.split para separar o array em vários caracteres.
+//Método reverse para reverter  palavra que irei chamar.
+//Método Join para juntar novamente os caracteres que foram separados nessa STRING.
+//Fiz o console.log para chamar a function e botei o nome que desejava, e com isso ele se inverteu.
