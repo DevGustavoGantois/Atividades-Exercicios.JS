18d585fa26f4795557ec10c43c75b7e5c76019c5
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-15 21:13:26 -0300

    Add files via upload

---
+++ Implemente uma função que receba um número e informe o quadrado desse número.js
@@ -0,0 +1,11 @@
+function QuadradoNumero (numero) {
+
+    return numero**2
+} 
+
+console.log(QuadradoNumero(8))
+
+//Declarei a FUNCTION quadradoNumero. Botei a váriavel número dentro da function
+//retornei o número ao quadrado
+//Dei um console.log, botei a function dentro, com o número 8 e o resultado foi 8x8 = 64
+
