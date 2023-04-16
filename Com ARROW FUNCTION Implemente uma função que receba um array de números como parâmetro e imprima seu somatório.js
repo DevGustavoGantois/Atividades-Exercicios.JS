ccbd011cd3c8bedeadc904f1233acbe868408f73
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-16 13:50:21 -0300

    Add files via upload

---
+++ Com ARROW FUNCTION Implemente uma função que receba um array de números como parâmetro e imprima seu somatório.js
@@ -0,0 +1,14 @@
+const soma = (n1,n2) => {
+    
+    
+    return n1 + n2;
+
+}  
+console.log(`A soma vai ser =`)
+console.log(soma(35 , 89))
+
+//Crie uma constante soma.
+//A constante soma será n1 e n2
+//Retornei o n1 e o n2, somando eles.
+//Usei o console.log para atribuir os valores as váriaveis n1 e n2
+//Mostrei o resultado.
