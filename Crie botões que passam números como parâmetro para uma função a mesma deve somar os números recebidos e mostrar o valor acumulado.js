ac74d881f2820f548c967907e4aa4792f69900d4
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-16 12:29:50 -0300

    Add files via upload

---
+++ Crie botões que passam números como parâmetro para uma função a mesma deve somar os números recebidos e mostrar o valor acumulado.js
@@ -0,0 +1,52 @@
+function Numero1(numero1) {
+    
+    return numero1
+}
+
+console.log('O valor do 1 número é =')
+console.log(Numero1(4))
+
+function Numero2(numero2) {
+
+    return numero2
+}
+
+console.log("O valor do 2 número é = ")
+console.log(Numero2(5))
+
+function Numero3(numero3) {
+
+    return numero3
+}
+
+console.log('O valor do 3 número é =')
+console.log(Numero3(6))
+ 
+function Numero4(numero4) {
+
+    return numero4
+}
+
+console.log('O valor do 4 número é =')
+console.log(Numero4(32))
+
+function Numero5(numero5) {
+
+    return numero5
+}
+
+console.log('O valor do 5 número é =')
+console.log(Numero5(22))
+
+function Soma(numero1, numero2, numero3, numero4, numero5) {
+
+    return numero1 + numero2 + numero3 + numero4 + numero5
+}
+
+console.log('O valor da soma desses número acumulados é =')
+
+console.log(Soma(Numero1(4), Numero2(5), Numero3(6), Numero4(32), Numero5(22)))
+
+//Criei várias funções retornando os números de cada um var numero1 , 2 , 3 , 4 , 5...
+//Criei uma função chamada soma que retornava todos os números das funções anteriores.
+//usei o console.log para exibir a soma dos números dessas funções.
