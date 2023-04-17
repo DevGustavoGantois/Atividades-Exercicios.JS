5f034bf09f1a1ba809e07df45925146f428fba58
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-16 23:01:56 -0300

    Add files via upload

---
+++ Crie uma função que receba dois parâmetros. A função deve calcular área do retângulo e retornar o resultado.js
@@ -0,0 +1,11 @@
+function calculaAreaRetangulo (largura, altura) {
+
+    return largura * altura;
+}
+
+console.log("A largura é 4 e a altura é 6 e a área será =  ")
+console.log(calculaAreaRetangulo(4, 6)); 
+
+//Criei uma função chamada 'calculaAreaRetangulo' que vai ter o parâmetro altura e largura.
+//Criei um return, retornando largura x altura.
+//Usei o console.log() para mostrar a resposta atribuindo valores aos 2 parâmetros.
