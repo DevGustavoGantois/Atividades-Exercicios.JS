d6e2ff65e1707275399422c6569ace034de12828
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-16 23:02:10 -0300

    Add files via upload

---
+++ COM ARROW FUNCTION Crie uma função que receba dois parâmetros, a função deve calcular área do retângulo e retornar o resultado.js
@@ -0,0 +1,13 @@
+const calculaAreaRetangulo = (largura, altura) => {
+
+
+    return largura * altura
+}
+
+console.log("A largura é 4 e a altura é 6 e a área será =")
+console.log(calculaAreaRetangulo(4,6))
+
+//Criei uma constante calculaAreaRetangulo
+//Criei 2 parâmetros, largura e altura
+//Retornei largura e altura multiplicando-os.
+//Usei o console.log() e atribui 2 valores para esses parâmetros.
