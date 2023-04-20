82cb2d62b236dbd52453da9d555ca655559948da
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-04-20 14:33:14 -0300

    Add files via upload

---
+++ sobre arrays 2.js
@@ -0,0 +1,25 @@
+//              1        2        3          4      5
+let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//alunos.push('Luiza');
+// alunos.push(`Eduardo`);
+
+// console.log(alunos.slice(0, -2));
+
+// alunos.push(`Luiza`); // Adiciona no fim
+// alunos.push(`Fábio`);
+
+//alunos[alunos.length] = `Luiza`; // adiciona no fim
+// alunos[alunos.length] = `Fábio`;
+// alunos[alunos.length] = `Luana`;
+
+// alunos[0] =`Eduardo`; // Altera
+// alunos[3] = `Luiza`; // Adicionado
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
